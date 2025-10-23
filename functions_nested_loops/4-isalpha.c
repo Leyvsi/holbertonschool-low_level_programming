5b6d919e0040abe2160ed71036e6ab0b7dbca16e
leyvsi <sylviecosta78500@gmail.com>
2025-10-23 13:09:32 +0000

    main.h actual

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - vérifie si un caractère est une lettre
+ * @c: le caractère à tester
+ *
+ * Retourne : 1 si c est une lettre (minuscule ou majuscule), 0 sinon
+ */
+int _isalpha(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return 1;
+    else
+        return 0;
+}
