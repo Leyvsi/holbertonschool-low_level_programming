4bf3612db540dc4abb77d915a932b215e6be810c
leyvsi <sylviecosta78500@gmail.com>
2025-10-23 11:51:28 +0000

    add .

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,22 @@
+<<<<<<< HEAD
 
 
+=======
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    char c;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+            _putchar(c);
+        _putchar('\n');
+    }
+}
+>>>>>>> 652ce70 (add .)
