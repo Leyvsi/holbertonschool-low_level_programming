87be479116e1589e030434ba06d7733e0f9f11ee
leyvsi <sylviecosta78500@gmail.com>
2025-10-27 14:13:52 +0000

    add 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+void puts2(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (i % 2 == 0)
+            write(1, &str[i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
