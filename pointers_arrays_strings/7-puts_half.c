cbc2d93f5dfd6c31298056185fb8b72e53460879
leyvsi <sylviecosta78500@gmail.com>
2025-10-27 14:19:19 +0000

    correct add 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,17 +3,18 @@
 
 void puts_half(char *str)
 {
-    int len = 0, start, i;
+	int len = 0, start, i;
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    start = len / 2;
-    if (len % 2 != 0)
-        start = (len + 1) / 2;
+	start = len / 2;
+	if (len % 2 != 0)
+		start = (len + 1) / 2;
 
-    for (i = start; i < len; i++)
-        write(1, &str[i], 1);
+	for (i = start; i < len; i++)
+		write(1, &str[i], 1);
 
-    write(1, "\n", 1);
+	write(1, "\n", 1);
 }
+
