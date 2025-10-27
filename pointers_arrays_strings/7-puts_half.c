70346625e335f573552d5865d58a7fe69ea7dee6
leyvsi <sylviecosta78500@gmail.com>
2025-10-27 14:16:29 +0000

    add 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+
+void puts_half(char *str)
+{
+    int len = 0, start, i;
+
+    while (str[len] != '\0')
+        len++;
+
+    start = len / 2;
+    if (len % 2 != 0)
+        start = (len + 1) / 2;
+
+    for (i = start; i < len; i++)
+        write(1, &str[i], 1);
+
+    write(1, "\n", 1);
+}
