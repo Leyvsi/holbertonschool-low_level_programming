6ddb258ffb2e2f592bfeef9bc9f183ada04e9ab5
leyvsi <sylviecosta78500@gmail.com>
2025-10-27 14:47:25 +0000

    add 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - convertit une chaîne en entier
+ * @s: chaîne à convertir
+ * Return: entier converti à partir de la chaîne
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
